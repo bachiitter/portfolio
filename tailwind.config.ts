132019fba03d45d58f3875310bacb379cb20c459
Bachitter <officialbachitterchahal@gmail.com>
2023-05-10 06:37:30 -0700

    Add spin-slow animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,12 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 2s linear infinite',
+      }
+    }
+  },
   plugins: [],
 } satisfies Config;
