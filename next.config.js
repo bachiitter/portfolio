061f5ff659043b13741dd1dd61401bdaa89206d2
Bachitter <officialbachitterchahal@gmail.com>
2023-05-10 06:18:18 -0700

    Update HomePage

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig = {
-  output: "export",
+  output: isDev ? "standalone" : "export",
   experimental: {
     typedRoutes: true,
-  }
-}
+  },
+  // async rewrites() {
+  //   return [{
+  //     source: '/api/v1/:path*',
+  //     destination: 'http://127.0.0.1:4000/api/v1/:path*'
+  //   }]
+  // }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,64 @@
+import { NowPlaying } from "@/components/NowPlaying";
+import pfp from "../assets/pfp.jpg";
+import Image from "next/image";
+
 export default function Home() {
-  return <main></main>;
+
+  return (
+    <main className="fixed inset-0 flex h-full w-full items-center justify-center xs:bg-neutral-100">
+      <div className="h-[calc(100% - 48px)] w-[calc(100% - 48px)] flex max-h-[600px] max-w-[400px] flex-col gap-4 rounded-[48px] bg-white p-10 xs:p-14">
+        <div className="relative w-fit">
+          <Image
+            src={pfp}
+            alt="Image of Bachitter"
+            width={64}
+            height={64}
+            className="rounded-full"
+          />
+        </div>
+        <div className="mb-6">
+          <h1 className="mb-2 text-2xl font-bold text-neutral-800">
+            Hello! I&#39;m Bachitter.
+          </h1>
+          <p className="font-medium text-neutral-600">
+            Software engineer, building dumb stuff on the internet.
+          </p>
+        </div>
+        <ul className="flex flex-col gap-4 text-neutral-600">
+          <li className="flex items-center gap-2">
+            <div className="text-xl">📍</div>
+            <p>
+              Lives in <span className="font-semibold">Vancouver, Canada</span>
+            </p>
+          </li>
+          <li className="flex items-center gap-2">
+            <div className="text-xl">🏗️</div>
+            <p>
+              Building{" "}
+              <span className="font-semibold">
+                <a href="https://dayplanr.bachitter.dev">Dayplanr</a>
+              </span>
+            </p>
+          </li>
+          <li className="flex items-center gap-2">
+            <NowPlaying />
+          </li>
+        </ul>
+        <div className="w-[calc(100% - 64px)] mt-16 flex h-[48px] flex-col gap-4 xs:flex-row">
+          <a
+            className="block flex-1 rounded-[24px] bg-neutral-100 text-center font-semibold leading-[48px]"
+            href="https://twitter.com/bachiitter"
+          >
+            Twitter
+          </a>
+          <a
+            className="block flex-1 rounded-[24px] bg-neutral-100 text-center font-semibold leading-[48px]"
+            href="mailto:me@bachitter.dev"
+          >
+            Email
+          </a>
+        </div>
+      </div>
+    </main>
+  )
 }
