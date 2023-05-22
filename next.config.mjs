c1b9b98ef4aa8431b450a0020c721495bf8bc488
Bachitter <officialbachitterchahal@gmail.com>
2023-05-22 14:31:23 -0700

    Remove rewrites and update deps

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const isDev = process.env.NODE_ENV === "development";
 /** @type {import('next').NextConfig} */
 const config = {
   output: isDev ? "standalone" : "export",
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: "http://127.0.0.1:4000/api/v1/:path*",
-      },
-    ];
-  },
 };
 
 export default config;
