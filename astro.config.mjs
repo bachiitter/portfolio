1b2557258a81c1eae386a61a1d9d98faad17e362
Bachitter <officialbachitterchahal@gmail.com>
2023-07-06 19:50:14 -0700

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://bachitter.dev",
+  integrations: [react(), tailwind(), sitemap()],
 });
