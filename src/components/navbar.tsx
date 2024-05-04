25091ce97a372bc248747a6dee613f6599599c21
nèiliǎn de <143877686+neiilian@users.noreply.github.com>
2024-05-04 15:02:41 -0700

    add it to nav bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,27 +1,34 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { ThemeSwitcher } from "./theme-switcher";
 
 const linkItems: Array<{
   name: string;
   href: string;
 }> = [
-    {
-      name: "about",
-      href: "/",
-    },
-    {
-      name: "blog",
-      href: "/blog",
-    },
-  ];
+  {
+    name: "about",
+    href: "/",
+  },
+  {
+    name: "blog",
+    href: "/blog",
+  },
+];
 
 export function Navbar() {
   return (
-    <nav className="bg-background w-full py-2 flex gap-2">
-      {linkItems.map((link) => (
-        <Link href={link.href} key={link.href}>
-          {link.name}
-        </Link>
-      ))}
+    <nav className="bg-background w-full py-2 flex gap-2 justify-between items-center">
+      <div className="flex gap-2">
+        {linkItems.map((link) => (
+          <Button variant="link" asChild key={link.href} className="p-0">
+            <Link href={link.href}>{link.name}</Link>
+          </Button>
+        ))}
+      </div>
+      <div>
+        <ThemeSwitcher />
+      </div>
     </nav>
   );
 }
