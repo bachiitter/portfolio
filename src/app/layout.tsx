70afcbf41f698f4b1e7356d495d82fd652ef38fa
nèiliǎn de <143877686+neiilian@users.noreply.github.com>
2024-05-06 15:27:58 -0700

    change min-h-screen to min-h-dvh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+      <body className={cn("min-h-dvh bg-background font-sans antialiased", fontSans.variable)}>
         <ThemeProvider
           defaultTheme="system"
           attribute="class"
