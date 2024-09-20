5e554008eed1ba80db3d23b6ecf3a45ce9f20457
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-09-20 03:37:24 -0700

    init astro

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://bachitter.dev",
+  integrations: [
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
