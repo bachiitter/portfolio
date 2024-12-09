3451588cf168a278e1c667403fea946383b66819
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-12-09 04:40:49 -0800

    remove dark mode, change blog page and post style

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,8 @@
 export function formatDate(value: Date) {
   return new Intl.DateTimeFormat("en-US", {
-    month: "2-digit",
+    month: "short",
     day: "2-digit",
+    year: "numeric",
     timeZone: "UTC",
   }).format(value);
 }
