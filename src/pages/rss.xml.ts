6913de75cc5401327f7989f4660d9fdbd09b6032
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-01-13 04:27:40 -0800

    sync

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,22 +2,19 @@ import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
 
-export const GET = import.meta.env.PROD
-  ? async (context: APIContext) => {
-      const posts = await getCollection("blog");
+export const GET = async (context: APIContext) => {
+  const posts = await getCollection("blog");
 
-      return rss({
-        title: "Bachitter",
-        description: "Hi! I am Bachitter Chahal, a Product Engineer based in Vancouver, Canada.",
-        site: context.site?.toString() || "",
-        // copyright: `© ${new Date().getFullYear()} Bachitter`,
-        items: posts.map((post) => ({
-          title: post.data.title || "",
-          pubDate: new Date(post.data.publishedAt || ""),
-          description: post.data.description || "",
-          link: `/blog/${post.data.slug}/`,
-        })),
-        customData: "<language>en-us</language>",
-      });
-    }
-  : null;
+  return rss({
+    title: "Bachitter",
+    description: "Hi! I am Bachitter Chahal, a Product Engineer based in Vancouver, Canada.",
+    site: context.site?.toString() || "",
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: new Date(post.data.publishedAt || ""),
+      link: `/blog/${post.data.slug}/`,
+    })),
+    customData: "<language>en-us</language>",
+  });
+};
