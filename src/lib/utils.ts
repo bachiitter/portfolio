8a18308d6d2f369989e6619a67f429e5187284bc
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-01-12 23:38:25 -0800

    change post date to year

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
 export function formatDate(value: Date) {
   return new Intl.DateTimeFormat("en-US", {
-    month: "short",
-    day: "2-digit",
     year: "numeric",
     timeZone: "UTC",
   }).format(value);
