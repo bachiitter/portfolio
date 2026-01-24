afb3bcf202f7829da302a134d89a13a857370b44
Bachitter <143877686+bachiitter@users.noreply.github.com>
2026-01-23 19:54:12 -0800

    add image lightbox

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function formatDate(value: string | number | Date) {
   return new Intl.DateTimeFormat("en-US", {
     year: "numeric",
