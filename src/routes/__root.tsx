bccbedc392b38732ad030a9318363ef10e6cc684
Bachitter <143877686+bachiitter@users.noreply.github.com>
2026-01-29 15:31:47 -0800

    fix typo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -67,7 +67,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <ClientOnly>
           <script
             defer
-            data-host-url={INFO.siteUrl}
+            data-domains={INFO.siteUrl}
             src="/stats.js"
             data-website-id="cfd2d291-8e0a-4160-b788-434caca658aa"
             data-umami-event="outbound-link-click"
